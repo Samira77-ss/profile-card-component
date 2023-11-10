88c0ff006e723798e3be6125ba43f043d7f8677a
Samira77-ss <samiraseyfii0709@gmail.com>
2023-11-10 17:10:22 +0330

    Added new changes

--- src/App.js
+++ src/App.js
@@ -6,34 +6,37 @@ function App() {
     <div className="App">
       <div className="container">
         <div className="card">
-          <div className="headerImage">
-            <img src={headerImage} alt="headerImage"></img>
+          <div className="headerBackground">
+            <img
+              src={headerImage}
+              className="headerImage"
+              alt="headerImage"
+            ></img>
           </div>
           <div className="profileContent">
-            <img src={victor} alt="victor" />
+            <img src={victor} alt="victor" className="profileImage" />
             <div className="content">
-              <div>
+              <div className="info">
                 <span className="name">Victor Crest</span>
                 <span className="age">26</span>
               </div>
               <p className="city">London</p>
             </div>
+            <hr />
           </div>
-        </div>
-        <div className="row">
-          <div className="col-4">
-            80K
-            <br />
-            Followers
-          </div>
-          <div className="col-4">
-            803K <br />
-            Likes
-          </div>
-          <div className="col-4">
-            1.4K
-            <br />
-            Photos
+          <div className="row">
+            <div className="col-4">
+              <span className="number">80K</span>
+              <span className="string">Followers</span>
+            </div>
+            <div className="col-4">
+              <span className="number">803K</span>
+              <span className="string">Likes</span>
+            </div>
+            <div className="col-4">
+              <span className="number">1.4K</span>
+              <span className="string">Photos</span>
+            </div>
           </div>
         </div>
       </div>
